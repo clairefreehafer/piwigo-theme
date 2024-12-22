cb3f605f7adb2b9de429b02ece78332b706f534e
clairefreehafer <22858381+clairefreehafer@users.noreply.github.com>
2024-12-22 14:53:01 -0500

    first commit

---
+++ themeconf.inc.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+Theme Name: mine
+Version: 0.1
+Description: me theme
+Theme URI:
+Author: Claire Freehafer
+Author URI:
+*/
+
+$themeconf = array(
+  'name'         => 'default',
+  'icon_dir'      => 'themes/default/icon',
+  'img_dir'      => 'themes/default/images',
+  'mime_icon_dir' => 'themes/default/icon/mimetypes/',
+  'local_head'    => 'local_head.tpl',
+);
+
+?>
